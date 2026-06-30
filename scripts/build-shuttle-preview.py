213b099a0ca776cb748ec1233bd91e5106753a62
Trevor Keenan <keenan_trevor@yahoo.ie>
2026-06-30 13:06:31 -0700

    Update build-shuttle-preview.py

--- scripts/build-shuttle-preview.py
+++ scripts/build-shuttle-preview.py
@@ -64,8 +64,8 @@ REQUIRES_ICOS_LICENSE_REASON = "requires_icos_license_acceptance_or_auth"
 ICOS_UNAUTHENTICATED_SUCCESS_REASON = "unauthenticated ICOS licence_accept download succeeded"
 FILL_VALUES = {"", "NA", "NAN", "NULL", "NONE", "-9999", "-9999.0", "-9999.00", "-6999", "-6999.0"}
 NOTICE_TEXT = (
-    "This is a lightweight visualization preview generated from FLUXNET Shuttle products. "
-    "For analysis, download and cite the official data product."
+    "This is a lightweight visualization preview of a subset of the data available. "
+    "For analysis, download the full official data product."
 )
 
 CANONICAL_TARGET_VARIABLES = [
